7807a5c71fb36a216c0b9d50c52d7654efb7bf28
Aondo_Chris <Chris@firmaflowledge.com>
2025-11-17 12:23:49 +0100

    added dashboard and page layout

--- Firma_Flow_React/src/App.jsx
+++ Firma_Flow_React/src/App.jsx
@@ -1,20 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/auth/Signup';
 import EmailVerification from './pages/auth/EmailVerification';
 import Login from './pages/auth/Login';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/email-verification" element={<EmailVerification />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<div className="min-h-screen bg-gray-100 flex items-center justify-center pt-24"><h1 className="text-4xl font-bold text-gray-800">Dashboard Coming Soon</h1></div>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
-    </Router>
   );
 }
 
---
+++ Firma_Flow_React/src/components/pageData.js
@@ -0,0 +1,46 @@
+export const pageData = [
+    {
+        name: "Dashboard",
+        path: "/dashboard",
+    },
+    {
+        name: "Customers",
+        path: "/customers",
+    },
+    {
+        name: "Suppliers",
+        path: "/suppliers",
+    },
+    {
+        name: "Inventory",
+        path: "/inventory",
+    },
+    {
+        name: "Sales",
+        path: "/sales",
+    },
+    {
+        name: "Payments",
+        path: "/payments",
+    },
+    {
+        name: "Purchases",
+        path: "/purchases",
+    },
+    {
+        name: "Expenses",
+        path: "/expenses",
+    },
+    {
+        name: "Reports",
+        path: "/reports",
+    },
+    {
+        name: "Advanced Reports",
+        path: "/advance-reports",
+    },
+    {
+        name: "Settings",
+        path: "/settings",
+    },
+];
